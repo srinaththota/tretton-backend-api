b5851ea836d420dc877c51a7a35f3219c3af3d43
srinaththota <srinath.thota@gmail.com>
2022-03-25 15:24:39 +0530

    added wild card to read all base packages

--- src/main/java/com/tretton/assignment/trettonbackendapi/TrettonBackendApiApplication.java
+++ src/main/java/com/tretton/assignment/trettonbackendapi/TrettonBackendApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.tretton.assignment.controller")
+@ComponentScan(basePackages = "com.tretton.assignment.*")
 public class TrettonBackendApiApplication {
 
 	public static void main(String[] args) {
